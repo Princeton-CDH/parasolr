2deffea6b0675496c89aa6feb59f74c6dd2257dc
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2019-01-30 09:41:24 -0500

    Fix breaking errors in schema.py #10

--- parasol/solr/schema.py
+++ parasol/solr/schema.py
@@ -10,7 +10,7 @@ class Schema:
             'Content-type': 'application/json'
         }
 
-    def _post_field(self, method, field_definition):
+    def _post_field(self, method, **field_kwargs):
         '''Post a field definition to the schema API'''
         data = {
             method: field_kwargs
@@ -24,7 +24,7 @@ class Schema:
 
     def add_field(self, **field_kwargs):
         '''Add a field with the supplied kwargs (or dict as kwargs)'''
-        self._post_field('add-field', field_kwargs)
+        self._post_field('add-field', **field_kwargs)
 
     def delete_field(self, name):
         '''Delete a field with the supplied kwargs (or dict as kwargs)'''
@@ -33,23 +33,23 @@ class Schema:
     def replace_field(self, **field_kwargs):
         '''Replace a field with the supplied kwargs (or dict as kwargs)'''
         # NOTE: Requires a full field definition, no partial updates
-        self._post_field('replace-field', field_kwargs)
+        self._post_field('replace-field', **field_kwargs)
 
     def add_copy_field(self, source, dest, max_chars=None):
         field_definition = {
-            'source': source
+            'source': source,
             'dest': dest
         }
         if max_chars:
             field_definition.max_chars = max_chars
-        self._post_field('add-copy-field', field_defintion)
+        self._post_field('add-copy-field', field_definition)
 
     def delete_copy_field(self, source, dest):
         self._post_field('delete-copy-field', {'source': source, 'dest': dest})
 
     def add_field_type(self, **field_kwargs):
         '''Add a field type to the Solr collection or core.'''
-        self._post_field('add-field-type', field_kwargs)
+        self._post_field('add-field-type', **field_kwargs)
 
     def delete_field_type(self, name):
        '''Delete a field type from the Solr collection or core'''
@@ -58,7 +58,7 @@ class Schema:
     def replace_field_type(self, **field_kwargs):
         '''Provide a full definition to replace a field'''
         # NOTE: Requires a full field-type definition, no partial updates
-        self._post_field('replace-field-type', field_kwargs)
+        self._post_field('replace-field-type', **field_kwargs)
 
     def get_schema(self):
         '''Get the full schema for a Solr collection or core.'''
