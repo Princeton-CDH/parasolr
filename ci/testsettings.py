5566b62bb79c182348af4d7e896beab73911986a
Benjamin Hicks <benjamin.w.hicks@gmail.com>
2019-02-11 13:55:03 -0500

    Extract test settings from being hardcoded #13

--- ci/testsettings.py
+++ ci/testsettings.py
@@ -16,3 +16,15 @@ INSTALLED_APPS = (
 # Keeping this blank avoids false detections from SECRET_KEY
 # repository checkers.
 # SECRET_KEY = ''
+
+
+# Default CI test settings; imported by parasol.solr.test_solr as testsettings
+# from top level project folder
+SOLR_CONNECTIONS = {
+    'test': {
+        'solr_url': 'http://localhost:8983/solr/',
+        'collection': 'parasol_test',
+        # aggressive commitWithin for test only
+        'commitWithin': 750
+    }
+}
