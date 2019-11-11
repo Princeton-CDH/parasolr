2474083b5954e9aed7251cfe41f994dfbc9b48c1
rlskoeser <rebecca.s.koeser@princeton.edu>
2019-11-11 08:56:42 -0500

    Use a decorator for django-optional classes and methods

--- parasolr/django/queryset.py
+++ parasolr/django/queryset.py
@@ -8,6 +8,9 @@ import logging
 from typing import Optional
 
 from parasolr import query
+from parasolr.django.util import requires_django
+# SolrClient needed for type annotation when testing without django
+from parasolr.solr.client import SolrClient
 
 try:
     from parasolr.django.solrclient import SolrClient
@@ -19,23 +22,23 @@ except ImportError:
 logger = logging.getLogger(__name__)
 
 
-if django:
+@requires_django
+class SolrQuerySet(query.SolrQuerySet):
+    """:class:`~parasolr.query.SolrQuerySet` subclass that
+    will automatically use :class:`~parasolr.django.SolrClient` if
+    no solr client is passed on.
 
-    class SolrQuerySet(query.SolrQuerySet):
-        """:class:`~parasolr.query.SolrQuerySet` subclass that
-        will automatically use :class:`~parasolr.django.SolrClient` if
-        no solr client is passed on.
+    Args:
+        Optional :class:`parasolr.solr.client.SolrClient`
+    """
 
-        Args:
-            Optional :class:`parasolr.solr.client.SolrClient`
-        """
-
-        def __init__(self, solr: Optional[SolrClient] = None):
-            # use passed-in solr client if there is one;
-            # otherwise, initialize a django solr client
-            super().__init__(solr or SolrClient())
+    def __init__(self, solr: Optional[SolrClient] = None):
+        # use passed-in solr client if there is one;
+        # otherwise, initialize a django solr client
+        super().__init__(solr or SolrClient())
 
 
-    class AliasedSolrQuerySet(SolrQuerySet, query.AliasedSolrQuerySet):
-        """Combination of :class:`SolrQuerySet` and
-        :class:`~parasolr.query.alias_queryset.AliasedSolrQuerySet`"""
+@requires_django
+class AliasedSolrQuerySet(SolrQuerySet, query.AliasedSolrQuerySet):
+    """Combination of :class:`SolrQuerySet` and
+    :class:`~parasolr.query.alias_queryset.AliasedSolrQuerySet`"""
