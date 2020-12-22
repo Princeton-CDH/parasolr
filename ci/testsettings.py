c7d8edb2e70db0e15e081d24a71f8aa642c664f2
rlskoeser <rebecca.s.koeser@princeton.edu>
2020-12-22 09:41:56 -0500

    Add GitHub actions unit test workflow with matrix strategy

--- ci/testsettings.py
+++ ci/testsettings.py
@@ -27,11 +27,11 @@ INSTALLED_APPS = (
 SOLR_CONNECTIONS = {
     'default': {
         # default config for testing pytest plugin
-        'URL': 'http://localhost:8983/solr/',
+        'URL': 'http://solr:8983/solr/',
         'COLLECTION': 'myplugin',
         'CONFIGSET': configset,
         'TEST': {
-            'URL': 'http://localhost:8983/solr/',
+            'URL': 'http://solr:8983/solr/',
             'COLLECTION': 'parasolr_test',
             # aggressive commitWithin for test only
             'COMMITWITHIN': 750,
