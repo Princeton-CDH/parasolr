f67570f71cf139407df196f3efce9567ddee7849
rlskoeser <rebecca.s.koeser@princeton.edu>
2021-01-21 15:31:18 -0500

    Set develop version to 0.7-dev

--- parasolr/__init__.py
+++ parasolr/__init__.py
@@ -1,7 +1,7 @@
 
 default_app_config = 'parasolr.apps.ParasolConfig'
 
-__version_info__ = (0, 6, 1, None)
+__version_info__ = (0, 7, 0, 'dev')
 
 # Dot-connect all but the last. Last is dash-connected if not None.
 __version__ = '.'.join([str(i) for i in __version_info__[:-1]])
